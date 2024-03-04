4ac3da674574b6af7148d62e997ef43de2d9a714
Shantanu Mane <shantanu.mane.200@outlook.com>
2024-03-04 19:40:11 +0530

    refactor(lint): files linted

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -1,47 +1,47 @@
-import { hostname } from "os";
+import { hostname } from 'os';
 
-const env = require("dotenv");
+const env = require('dotenv');
 env.config({
-	path: process.env.NODE_ENV === "development" ? ".env.local" : ".env",
+  path: process.env.NODE_ENV === 'development' ? '.env.local' : '.env',
 });
 
 const {
-	NODE_ENV,
-	APP_PORT,
-	APP_HOST,
-	DB_HOST,
-	MYSQL_DB_PORT,
-	POSTGRES_DB_PORT,
-	DB_USER,
-	DB_PASSWORD,
-	DB_NAME,
-	KAFKA_HOST,
-	KAFKA_PORT,
-	KAFKA_CLIENT_ID,
-	KAFKA_GROUP_ID,
-	KAFKA_TOPIC,
-	JWT_SECRET,
+  NODE_ENV,
+  APP_PORT,
+  APP_HOST,
+  DB_HOST,
+  MYSQL_DB_PORT,
+  POSTGRES_DB_PORT,
+  DB_USER,
+  DB_PASSWORD,
+  DB_NAME,
+  KAFKA_HOST,
+  KAFKA_PORT,
+  KAFKA_CLIENT_ID,
+  KAFKA_GROUP_ID,
+  KAFKA_TOPIC,
+  JWT_SECRET,
 } = process.env;
 
 export const envConfig = {
-	ENV: NODE_ENV,
-	PORT: APP_PORT,
-	HOST: APP_HOST,
-	HOSTNAME: hostname(),
-	DB: {
-		DB_HOST: DB_HOST,
-		MYSQL_DB_PORT: MYSQL_DB_PORT,
-		POSTGRES_DB_PORT: POSTGRES_DB_PORT,
-		DB_USER: DB_USER,
-		DB_PASSWORD: DB_PASSWORD,
-		DB_NAME: DB_NAME,
-	},
-	KAFKA: {
-		KAFKA_HOST: KAFKA_HOST,
-		KAFKA_PORT: KAFKA_PORT,
-		KAFKA_CLIENT_ID: KAFKA_CLIENT_ID,
-		KAFKA_GROUP_ID: KAFKA_GROUP_ID,
-		KAFKA_TOPIC: KAFKA_TOPIC,
-	},
-	JWT_SECRET_KEY: JWT_SECRET,
+  ENV: NODE_ENV,
+  PORT: APP_PORT,
+  HOST: APP_HOST,
+  HOSTNAME: hostname(),
+  DB: {
+    DB_HOST: DB_HOST,
+    MYSQL_DB_PORT: MYSQL_DB_PORT,
+    POSTGRES_DB_PORT: POSTGRES_DB_PORT,
+    DB_USER: DB_USER,
+    DB_PASSWORD: DB_PASSWORD,
+    DB_NAME: DB_NAME,
+  },
+  KAFKA: {
+    KAFKA_HOST: KAFKA_HOST,
+    KAFKA_PORT: KAFKA_PORT,
+    KAFKA_CLIENT_ID: KAFKA_CLIENT_ID,
+    KAFKA_GROUP_ID: KAFKA_GROUP_ID,
+    KAFKA_TOPIC: KAFKA_TOPIC,
+  },
+  JWT_SECRET_KEY: JWT_SECRET,
 };
--- src/helpers/kafka.helper.js
+++ src/helpers/kafka.helper.js
@@ -1,12 +1,12 @@
-import { Kafka } from "kafkajs";
-import { logger } from "#helpers/index";
-import { envConfig } from "#configs/index";
+import { Kafka } from 'kafkajs';
+import { logger } from '#helpers/index';
+import { envConfig } from '#configs/index';
 
 const options = {
-	clientId: envConfig.KAFKA.KAFKA_CLIENT_ID,
-	brokers: ["localhost:9092"],
-	logLevel: 1,
-	ssl: false,
+  clientId: envConfig.KAFKA.KAFKA_CLIENT_ID,
+  brokers: ['localhost:9092'],
+  logLevel: 1,
+  ssl: false,
 };
 
 const kafka = new Kafka(options);
@@ -15,7 +15,7 @@ const admin = kafka.admin();
 
 const producer = kafka.producer();
 const consumer = kafka.consumer({
-	groupId: envConfig.KAFKA.KAFKA_GROUP_ID,
+  groupId: envConfig.KAFKA.KAFKA_GROUP_ID,
 });
 
 /**
@@ -23,54 +23,54 @@ const consumer = kafka.consumer({
  * @return {Promise<void>}
  */
 async function initKafka() {
-	try {
-		admin
-			.connect()
-			.then(() => {
-				logger.log("verbose", "Kafka admin connected");
-			})
-			.catch((error) => {
-				logger.log("error", error);
-			});
+  try {
+    admin
+        .connect()
+        .then(() => {
+          logger.log('verbose', 'Kafka admin connected');
+        })
+        .catch((error) => {
+          logger.log('error', error);
+        });
 
-		admin
-			.createTopics({
-				topics: [{ topic: envConfig.KAFKA.KAFKA_TOPIC }],
-			})
-			.then(() => {
-				logger.log("verbose", `Topic created ${envConfig.KAFKA.KAFKA_TOPIC}`);
-			})
-			.catch((error) => {
-				logger.log("error", error);
-			});
+    admin
+        .createTopics({
+          topics: [{ topic: envConfig.KAFKA.KAFKA_TOPIC }],
+        })
+        .then(() => {
+          logger.log('verbose', `Topic created ${envConfig.KAFKA.KAFKA_TOPIC}`);
+        })
+        .catch((error) => {
+          logger.log('error', error);
+        });
 
-		await producer.connect();
-		await consumer.connect();
-		logger.log("verbose", "Kafka initialized successfully!");
-	} catch (e) {
-		// logger.log('error', e);
-		throw e;
-	}
+    await producer.connect();
+    await consumer.connect();
+    logger.log('verbose', 'Kafka initialized successfully!');
+  } catch (e) {
+    // logger.log('error', e);
+    throw e;
+  }
 }
 
 module.exports = {
-	producer,
-	consumer,
-	admin,
-	initKafka,
-	produceMessage: async (topic, message) => {
-		await producer.send({
-			topic,
-			messages: [{ value: JSON.stringify(message) }],
-		});
-	},
-	consumeMessage: async (topic, callback) => {
-		await consumer.subscribe({ topic, fromBeginning: true });
+  producer,
+  consumer,
+  admin,
+  initKafka,
+  produceMessage: async (topic, message) => {
+    await producer.send({
+      topic,
+      messages: [{ value: JSON.stringify(message) }],
+    });
+  },
+  consumeMessage: async (topic, callback) => {
+    await consumer.subscribe({ topic, fromBeginning: true });
 
-		await consumer.run({
-			eachMessage: async ({ topic, partition, message }) => {
-				callback(message);
-			},
-		});
-	},
+    await consumer.run({
+      eachMessage: async ({ topic, partition, message }) => {
+        callback(message);
+      },
+    });
+  },
 };
